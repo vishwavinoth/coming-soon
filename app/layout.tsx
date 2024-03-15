095d07b5a72ca0458065c62567da6f36c6635d30
vishwa offl <66408627+vishwavinoth@users.noreply.github.com>
2024-03-16 01:28:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "typesafe",
-  description: "Embrace Type-Safety! Use TypeScript to its fullest.",
+  title: "Wide Crew",
+  description: "WideCrew offers professional web design and development services, tailored to your business needs. From responsive design to custom development, we create user-friendly websites that drive results.",
 };
 
 export default function RootLayout({
@@ -16,9 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} max-w-screen max-h-screen overflow-x-hidden overflow-y-hidden`}
-      >
+      <head>
+        <meta charSet="UTF-8" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <style>{inter.css}</style>
+      </head>
+      <body className={`${inter.className} max-w-screen max-h-screen overflow-x-hidden overflow-y-hidden`}>
         {children}
       </body>
     </html>
